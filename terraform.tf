342e9a9c22e4e0f83affa5f6ff7bb34ef2b4c62f
jamesmonia <44216507+jamesmonia@users.noreply.github.com>
2023-05-01 19:54:33 -0400

    added outputs, terraform.tf, variables, tfvars

---
+++ terraform.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_version = ">=1.0.0"
+  /*
+  backend "s3" {
+    bucket = "backend-test-jm"
+    key    = "prod/state"
+    region = "us-east-1"
+  } */
+
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+    // all provider syntax can be found on the terraform.io registry
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.0"
+    }
+
+    // this was referenced previously in the main.tf file so this provider was probably already installed using the default/latest version
+    // best practice to require and specify a specific version
+    // can use init -upgrade to downgrade as well
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+
+    // used to create local files where terraform is run
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 3.1.0"
+    }
+
+  }
+}
---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+# Public Subnet Values
+// the value set in a .tfvars file takes precedence over an environment variable and the default value set in the variable block.
+
+variables_sub_auto_ip = true
+variables_sub_az      = "us-east-1d"
+variables_sub_cidr    = "10.0.204.0/24"
